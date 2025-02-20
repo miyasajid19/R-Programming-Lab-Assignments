4349384cae29a47728d2c3c8d24c5aa3c22c0123
Sajid Miya <166320427+miyasajid19@users.noreply.github.com>
2025-02-21 02:05:20 +0530

    temp

--- Experiment 2/Question_5.r
+++ Experiment 2/Question_5.r
@@ -1,11 +1,11 @@
 Mode <- function(x) {
     unique_values <- unique(x)
     frequencies <- tabulate(match(x, unique_values))
-    mode_value <- unique_values[which.max(frequencies)]
-    mode_frequency <- max(frequencies)
-    return(list(mode = mode_value, frequency = mode_frequency))
+    max_frequency <- max(frequencies)
+    mode_values <- unique_values[frequencies == max_frequency]
+    return(list(modes = mode_values, frequency = max_frequency))
 }
 
-x = c(1, 2, 3, 4, 1, 2, 3, 1, 2, 3, 2, 1, 2, 1, 1, 3, 1, 2, 4, 4, 4, 1, 4)
+x = c(1, 2, 3, 4, 1, 2, 3, 1, 2, 3, 2, 1, 2, 1, 1,2,2, 3, 1, 2, 4, 4, 4, 1, 4)
 mode_result <- Mode(x)
-print(paste("The mode of the data is:", mode_result$mode, "with a frequency of:", mode_result$frequency))
+print(paste("The modes of the data are:", paste(mode_result$modes, collapse = ", "), "with a frequency of:", mode_result$frequency))
